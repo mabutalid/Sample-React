43c4665a318ae19b3ec44fcb2986ac0784b3fe84
mabutalid <theoaguilar123@gmail.com>
2021-01-04 19:35:59 +0800

    modified app v3

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to CDK deployed app/i);
+  const linkElement = screen.getByText(/Welcome to CDK/i);
   expect(linkElement).toBeInTheDocument();
 });
