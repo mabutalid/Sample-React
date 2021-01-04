f0db4658c7fd885aca931018b9289794af4835fc
mabutalid <theoaguilar123@gmail.com>
2021-01-04 16:34:48 +0800

    force fail again the test stage

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to CDK deployed apps</h1>
+        <h1>Welcome to CDK</h1>
         <h2> This a React App is hosted in Amazon S3 Bucket</h2>
         <h2> CodePipeline is used</h2>
       </header>
